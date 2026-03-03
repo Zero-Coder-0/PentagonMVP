90f041d003ef9af2e009e39c9472deab0eae2254
Zero Arshad <zeroarshadzero@gmail.com>
2026-03-04 00:01:03 +0530

    Initial commit of geoestate

---
+++ src/app/super/schema/page.tsx
@@ -0,0 +1,9 @@
+// app/super/schema/page.tsx
+export default function SchemaPage() {
+  return (
+    <main>
+      <h1>Schema Management</h1>
+      <p>Work in progress...</p>
+    </main>
+  );
+}
